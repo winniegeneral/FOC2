6ebaa280c7fec712bc31cc026193367e2fc24e86
winniegeneral <hyjerrytang@gmail.com>
2023-03-10 23:43:37 -0800

    Change sensor to AS5600

--- 01-Firmware/B-G431B-ESC1-v2/Core/Src/main.c
+++ 01-Firmware/B-G431B-ESC1-v2/Core/Src/main.c
@@ -49,7 +49,7 @@
 // Position sensor type :
 //    "AS5600_I2C"
 //    "AS5048A_PWM"
-#define SENSOR_TYPE AS5048A_PWM
+#define SENSOR_TYPE AS5600_I2C
 
 // PID loop period in µs
 //  normal setting is 1000us (1KHz)
